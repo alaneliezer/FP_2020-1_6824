cd8c990c05c2fb290d7373e6d572f8f521f20861
alaneliezer <54009334+alaneliezer@users.noreply.github.com>
2019-10-29 20:27:15 -0600

    subir

--- TAREAS/4/main.c
+++ TAREAS/4/main.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(int argc,char* argu[]){
-int lista[argc],suma=0;
+float lista[argc],suma=0;
 float promedio;
 for (int i=1;i<argc;i++){
-lista[i]=atoi(argu[i]);
+lista[i]=atof(argu[i]);
 }
 for (int i=1;i<argc;i++){
 suma=suma+lista[i];
 }
-promedio=(float)suma/(argc-1);
+promedio=suma/(argc-1);
 printf("%.2f\n",promedio);
 return 0;
 }
