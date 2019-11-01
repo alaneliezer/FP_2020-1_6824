cdd52da3ed5587d0227429802fdcf1b7fbc51e78
alan <@>
2019-11-01 00:55:43 -0600

    subir

--- TAREAS/6/main.c
+++ TAREAS/6/main.c
@@ -1,19 +1,16 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main(int argc,char* argu[]){
-    int resultado;
-    int argui[argc];
-    
-    for(int i=1;i<argc;i++){
-    resultado=1;
-    
-    for(int j=1;j<argc;j++){
-    argui[j]=atoi(argu[j]);
-    resultado=resultado*argui[j];
-    }
-    
-resultado=resultado/argui[i];
-printf("%i\n",resultado);
-}
-return 0;
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc,char*argu[]){
+	double resultado;
+	double arguf[argc];
+	for(int i=1;i<argc;i++){
+		resultado=1;
+		for(int j=1;j<argc;j++){
+			arguf[j]=atof(argu[j]);
+			resultado=resultado*arguf[j];
+		}
+		resultado=resultado/arguf[i];
+		printf("%.2f\n",resultado);
+	}
+	return 0;
 }
